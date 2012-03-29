399abefd8b4459c0ee37fc7aba1bf8abe3290bd2
Matthew Simo <matthew.a.simo@gmail.com>
2012-03-29 14:39:00 -0500

    Fix enqueue keyword for the /css/fonts.css stylesheet. Should fix error in /css/form.css not being enqueued.

--- functions.php
+++ functions.php
@@ -141,8 +141,7 @@ function storefrontal_styles(){
 	wp_enqueue_style('style',get_template_directory_uri() . "/style.css", false, THEME_VERSION, 'all');
 
 	if( $up_options['disable_custom_fonts'] == false )
-		wp_enqueue_style('form',get_template_directory_uri() . "/css/fonts.css", array('style'), THEME_VERSION, 'all');
-
+	wp_enqueue_style('fonts',get_template_directory_uri() . "/css/fonts.css", array('style'), THEME_VERSION, 'all');
 	wp_enqueue_style('all',get_template_directory_uri() . "/css/all.css", array('style'), THEME_VERSION, 'all');
 	wp_enqueue_style('print',get_template_directory_uri() . "/css/print.css", array('style'), THEME_VERSION, 'print');
 	wp_enqueue_style('form',get_template_directory_uri() . "/css/form.css", array('style'), THEME_VERSION, 'all');
